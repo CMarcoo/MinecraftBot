e5319feef8be65c9f3b6b7e9f25f9fdd841763dc
TheViperShow <29604693+TheViperShow@users.noreply.github.com>
2020-07-29 00:11:01 +0200

    Updating dumb Main.java

--- src/main/java/me/thevipershow/minecraftbot/Main.java
+++ src/main/java/me/thevipershow/minecraftbot/Main.java
@@ -11,6 +11,7 @@ import me.thevipershow.minecraftbot.packets.auth.LoginStartPacket;
 import me.thevipershow.minecraftbot.packets.auth.LoginSuccessPacket;
 import me.thevipershow.minecraftbot.packets.handshake.HandshakePacket;
 import me.thevipershow.minecraftbot.packets.handshake.PingPacket;
+import me.thevipershow.minecraftbot.packets.handshake.RequestPacket;
 
 /**
  * @author TheViperShow
@@ -27,56 +28,58 @@ public final class Main {
             println(s1);
     }
 
-    private static final ThreadLocalRandom rand = ThreadLocalRandom.current();
-
     public static void main(final String[] args) {
-        final ArgumentsHandler argsHandler = new ArgumentsHandler(args);
+        final ArgumentsHandler argsHandler = new ArgumentsHandler(args); // A simple object to parse the jvm arguments.
+        final String address = argsHandler.getAddress();
+        final int port = argsHandler.getPort();
 
         final int randomPort = 25999;
 
         try {
 
-            final InetAddress dstAddress = InetAddress.getByName(argsHandler.getAddress());
+            final InetAddress dstAddress = InetAddress.getByName(address);
             final Socket socket = new Socket();
-            socket.setSoTimeout(3500);
-            socket.setTcpNoDelay(true);
-            socket.setTrafficClass(18);
-            final InetSocketAddress inetSocketAddress = new InetSocketAddress(dstAddress, argsHandler.getPort());
-            socket.connect(inetSocketAddress);
-            println(String.format("Connected to destination Socket: [%s, %d]", argsHandler.getAddress(), argsHandler.getPort()));
+            socket.setSoTimeout(3500); // this sets a connection timeout, the connection will close if the server doesn't respond for more than 3.5 sec
+            socket.setTcpNoDelay(true); // Enable Nagle's algorithm
+            socket.setTrafficClass(18); // settings traffic class.
+            final InetSocketAddress inetSocketAddress = new InetSocketAddress(dstAddress, port); // The server we are going to connect to
+            socket.connect(inetSocketAddress); // connecting
 
-            final DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-            final DataInputStream dis = new DataInputStream(socket.getInputStream());
+            println(String.format("Connected to destination Socket: [%s, %d]", address, port));
 
-            final HandshakePacket handshake = new HandshakePacket(47, argsHandler.getAddress(), argsHandler.getPort(), HandshakePacket.HandshakeNextState.LOGIN);
-            final PingPacket pingPacket = new PingPacket();
+            final DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            final DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
 
-            handshake.sendPacket(dos);
 
-            pingPacket.sendPacket(dos);
+            // Initializing the HandshakePacket (https://wiki.vg/Protocol#Handshake)
+            // This packet is the first packet and will be sent to the server, telling him to start a login phase.
+            final HandshakePacket handshake = new HandshakePacket(0x2F, address, port, HandshakePacket.HandshakeNextState.LOGIN);  // 0x2F is 47, the protocol for 1.8-1.8.9
+            handshake.sendPacket(dataOutputStream); // sending the packet to the server
 
-            final LoginStartPacket loginStartPacket = new LoginStartPacket("CyberNoob");
+            // Initializing the RequestPacket (https://wiki.vg/Protocol#Request)
+            // This is an empty packet with id 0x00 and should be sent immediately after the Handshake.
+            final RequestPacket request = new RequestPacket();
+            request.sendPacket(dataOutputStream); // sending the packet to the server
 
-            println("Sending login start packet:");
-            loginStartPacket.sendPacket(dos);
+            // Initializing the LoginStartPacket (https://wiki.vg/Protocol#Login_Start)
+            // This makes the server start the authentication process, we will skip encryption as we're targeting offline-mode.
+            final LoginStartPacket loginStart = new LoginStartPacket("Skeppy");
+            loginStart.sendPacket(dataOutputStream); // sending the packet to the server
 
-            final int nextPacket = DataUtils.readVarInt(dis);
-            if (nextPacket == 0x02) {
+            // We'll start reading packets , if everything has worked we should receive a LoginSuccessPacket (https://wiki.vg/Protocol#Login_Success)
+            final int packetLength = DataUtils.readVarInt(dataInputStream); // we first read (Length of packet data + length of the packet ID)
+            final int packetID = DataUtils.readVarInt(dataInputStream); // Then we read the ID of the packet (VarInt).
 
-                println("Received LoginSuccessPacket.");
-                final LoginSuccessPacket loginSuccessPacket = new LoginSuccessPacket();
-                loginSuccessPacket.readData(dis);
+            println(packetLength + " " + packetID);
 
-                Thread.sleep(100);
-            } else {
-                println(String.format("Received unexpected packet with ID: %d", nextPacket));
-            }
 
-            dos.close();
-            dis.close();
+
+
+            dataOutputStream.close(); // closing all connections
+            dataInputStream.close();
             socket.close();
 
-        } catch (final IOException | InterruptedException e) {
+        } catch (final IOException e) {
             e.printStackTrace();
         }
     }
